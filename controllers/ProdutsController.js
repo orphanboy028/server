b0944a034ab2fa1c6ab525b73c5db08a96fd9a41
sanjay <orphanboy028@gmail.com>
2023-03-19 15:24:11 +0530

    create image upload galley

--- controllers/ProdutsController.js
+++ controllers/ProdutsController.js
@@ -4,12 +4,85 @@ const AppError = require("../utils/appError");
 const Produts = require("../models/ProductsModel");
 const multer = require("multer");
 
+// update feature image
+const multerstorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(
+      null,
+      path.resolve(`${__dirname}/../../frontend/public/product-gallery-images`)
+    );
+  },
+  filename: function (req, file, cb) {
+    const ext = file.mimetype.split("/")[1];
+    cb(null, `user-${req.user._id}-${Date.now()}.${ext}`);
+  },
+});
+
+const upload = multer({
+  storage: multerstorage,
+});
+
+exports.uploadProductFeatureImage = upload.single("images");
+exports.uploadProductsGalleryImages = upload.array("imageGallery", 12);
+
 exports.createProduct = catchAsync(async (req, res, next) => {
   const createProduct = await Produts.create(req.body);
 
   res.status(200).json({
     status: "Success",
     createProduct,
+  });
+});
+
+exports.updateFeatureImage = catchAsync(async (req, res, next) => {
+  const { productId, productname } = req.body;
+  const photoname = req.files.filename;
+
+  const fetureImage = await Produts.findByIdAndUpdate(
+    productId,
+    {
+      images: {
+        url: photoname,
+        altText: productname,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+
+  res.status(200).json({
+    status: "Success",
+    fetureImage,
+  });
+});
+
+exports.updateGalleryImage = catchAsync(async (req, res, next) => {
+  const { productId, productname } = req.body;
+  const files = req.files;
+
+  const images = [];
+
+  for (const file of files) {
+    images.push({
+      url: file.filename,
+      altText: productname,
+    });
+  }
+
+  const galleyImages = await Produts.findByIdAndUpdate(
+    productId,
+    {
+      imageGallery: images,
+    },
+    {
+      new: true,
+    }
+  );
+
+  res.status(200).json({
+    status: "Success",
+    galleyImages,
   });
 });
 
--- routes/ProductsRoute.js
+++ routes/ProductsRoute.js
@@ -7,5 +7,18 @@ router.route("/get-all-products").get(productController.getAllProducts);
 
 router.use(authController.protect, authController.restricTO("user"));
 router.route("/creat-product").post(productController.createProduct);
+router
+  .route("/update-feature-image")
+  .patch(
+    productController.uploadProductFeatureImage,
+    productController.updateFeatureImage
+  );
+
+router
+  .route("/update-Gallery-image")
+  .patch(
+    productController.uploadProductsGalleryImages,
+    productController.updateGalleryImage
+  );
 
 module.exports = router;
